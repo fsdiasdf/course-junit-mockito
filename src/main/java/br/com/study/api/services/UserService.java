60018cde9c81062a8ffaa58a098ae1fdbb43e66d
fsdiasdf <fsdiasdf@gmail.com>
2023-01-09 19:19:42 -0300

    Created new method findAll

--- src/main/java/br/com/study/api/services/UserService.java
+++ src/main/java/br/com/study/api/services/UserService.java
@@ -2,7 +2,10 @@ package br.com.study.api.services;
 
 import br.com.study.api.domain.User;
 
+import java.util.List;
+
 public interface UserService {
 
     User findById(Integer id);
+    List<User> findAll();
 }
