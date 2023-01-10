4398770cfd162a60dba8b65910b3c56bb4ffbad7
fsdiasdf <fsdiasdf@gmail.com>
2023-01-10 11:52:50 -0300

    Test Method Create User with Success

--- src/test/java/br/com/study/api/services/impl/UserServiceImplTest.java
+++ src/test/java/br/com/study/api/services/impl/UserServiceImplTest.java
@@ -15,6 +15,7 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.when;
 
@@ -86,7 +87,17 @@ class UserServiceImplTest {
     }
 
     @Test
-    void create() {
+    void whenCreateThenReturnSuccess() {
+        when(repository.save(any())).thenReturn(user);
+
+        User response = service.create(userDTO);
+
+        assertNotNull(response);
+        assertEquals(User.class, response.getClass());
+        assertEquals(ID, response.getId());
+        assertEquals(NAME, response.getName());
+        assertEquals(EMAIL, response.getEmail());
+        assertEquals(PASSWORD, response.getPassword());
     }
 
     @Test
